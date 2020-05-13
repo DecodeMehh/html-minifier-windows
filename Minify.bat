6a2584b88fa08a6cfbf18287f92e674fdf1207db
Tesla <43712252+0x11DFE@users.noreply.github.com>
2020-05-13 10:06:02 +0200

    Update Minify.bat
    escaping directory

--- Minify.bat
+++ Minify.bat
@@ -7,5 +7,5 @@ echo File = 2
 SET /P TYPE=Option: %=%
 SET /P INPUT=Target: %=%
 
-if %TYPE%==1 (node node_modules\html-minifier\cli.js -c config.json --input-dir %INPUT% --output-dir %INPUT%_minified)
-if %TYPE%==2 (node node_modules\html-minifier\cli.js -c config.json %INPUT% -o %INPUT%)
+if %TYPE%==1 (node node_modules\html-minifier\cli.js -c config.json --input-dir "%INPUT%" --output-dir "%INPUT%_minified")
+if %TYPE%==2 (node node_modules\html-minifier\cli.js -c config.json "%INPUT%" -o "%INPUT%")
